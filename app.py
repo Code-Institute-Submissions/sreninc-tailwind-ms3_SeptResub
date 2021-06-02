5f2036bbe4ee08cbbe468f2bc93b1a6bfdc40e75
sreninc <sreninc@gmail.com>
2021-06-02 17:30:56 +0000

    INITIAL SETUP & STRUCTURE COMMIT

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_assets import Bundle, Environment
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+assets = Environment(app)
+css = Bundle("css/main-dev.css", output="css/main.css", filters="postcss")
+
+assets.register("css", css)
+css.build()
+
+
+@app.route("/")
+@app.route("/index")
+def generate():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
