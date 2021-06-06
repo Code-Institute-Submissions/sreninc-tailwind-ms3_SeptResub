c91f14ec419ff92c9e88f8075927a7a52acf299e
sreninc <sreninc@gmail.com>
2021-06-06 14:21:19 +0000

    FEAT: Added app nav menu toggles to script

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,27 @@
 let mobileNav = document.getElementById("headerMobileNav");
-console.log(mobileNav);
 
 function toggleMobileMenu() {
     if (mobileNav.classList.contains("invisible")) {
         mobileNav.classList.remove("invisible");
     } else {
         mobileNav.classList.add("invisible");
+    }
+}
+
+function hideMobileMenu() {
+    if (mobileNav.classList.contains("hidden")) {
+        mobileNav.classList.remove("hidden");
+    } else {
+        mobileNav.classList.add("hidden");
+    }
+}
+
+let userMenu = document.getElementById("toggleUserMenu");
+
+function toggleUserMenu() {
+    if (userMenu.classList.contains("invisible")) {
+        userMenu.classList.remove("invisible");
+    } else {
+        userMenu.classList.add("invisible");
     }
 }
