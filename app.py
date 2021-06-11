869d87dc3ec57a13147a28f5041651dda773b2ac
sreninc <sreninc@gmail.com>
2021-06-11 20:02:03 +0000

    FEAT: Added signin function to login template

--- app.py
+++ app.py
@@ -88,6 +88,36 @@ def login():
     return render_template("login.html", title=title, header=header)
 
 
+@app.route("/signin", methods=["GET", "POST"])
+def signin():
+    if request.method == "POST":
+        # check if email exists in db
+        existing_user = mongo.db.users.find_one(
+            {"email": request.form.get("email").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["email"] = request.form.get("email").lower()
+                email = list(mongo.db.users.find({"email": session["email"]}))
+                flash("Welcome, {}".format(
+                    request.form.get("email")))
+                return dashboard()
+            else:
+                # invalid password match
+                flash("Incorrect Email and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # Email doesn't exist
+            flash("Incorrect Email and/or Password")
+            return redirect(url_for("login"))
+
+    return redirect(url_for("login"))
+
+
+
 @app.route("/dashboard")
 def dashboard():
     title = "Dashboard"
