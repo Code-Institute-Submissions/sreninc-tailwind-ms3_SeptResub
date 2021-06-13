cfc6e0298e3038edbc75cf1f79823736b577e93a
sreninc <sreninc@gmail.com>
2021-06-13 20:00:41 +0000

    FEAT: Updated update alerts to auto dismiss

--- static/js/script.js
+++ static/js/script.js
@@ -39,7 +39,32 @@ function filterBookings() {
 
 // To show popups
 function generateFlashMessage(message) {
-    Swal.fire(message);
+Swal.fire({
+  title: 'Success',
+  html: message,
+  timer: 1000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading()
+    timerInterval = setInterval(() => {
+      const content = Swal.getHtmlContainer()
+      if (content) {
+        const b = content.querySelector('b')
+        if (b) {
+          b.textContent = Swal.getTimerLeft()
+        }
+      }
+    }, 100)
+  },
+  willClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
 }
 
 function confirmDeleteBooking(id) {
