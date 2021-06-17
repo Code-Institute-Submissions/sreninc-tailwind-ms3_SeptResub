6773e4a1de5c9b73166196e42a465992bbbae159
sreninc <sreninc@gmail.com>
2021-06-17 20:18:07 +0000

    FEAT: Blocked manual url entry for logged/not logged in pages

--- app.py
+++ app.py
@@ -38,6 +38,9 @@ def index():
         "subTitle": "Manage your operations in one place and get guests when you need them most.",
         "displayButtons": "yes"
     }
+
+    if session.get("name"):
+        return redirect(url_for("dashboard"))
     return render_template("index.html", title=title, header=header)
 
 
@@ -50,6 +53,8 @@ def features():
         "subTitle": "Booking and guest management made to help you grow your restaurant.",
         "displayButtons": "yes"
     }
+    if session.get("name"):
+        return redirect(url_for("dashboard"))
     return render_template("features.html", title=title, header=header)
 
 
@@ -62,6 +67,8 @@ def contact():
         "subTitle": "Let us answer your questions and guide you in the right direction.",
         "displayButtons": "no"
     }
+    if session.get("name"):
+        return redirect(url_for("dashboard"))
     return render_template("contact.html", title=title, header=header)
 
 
@@ -74,6 +81,8 @@ def signup():
         "subTitle": "Choose the package that suits your restaurant and let's get started!",
         "displayButtons": "no"
     }
+    if session.get("name"):
+        return redirect(url_for("dashboard"))
     return render_template("signup.html", title=title, header=header)
 
 
@@ -86,6 +95,8 @@ def login():
         "subTitle": "We're glad you are back.",
         "displayButtons": "no"
     }
+    if session.get("name"):
+        return redirect(url_for("dashboard"))
     return render_template("login.html", title=title, header=header)
 
 
@@ -109,13 +120,12 @@ def signin():
                 flash("Welcome " + session['name'])
                 return redirect(url_for("dashboard"))
             else:
-                # invalid password match
-                flash("Incorrect Email and/or Password")
+                print("invalid")
                 return redirect(url_for("login"))
 
         else:
             # Email doesn't exist
-            flash("Incorrect Email and/or Password")
+            print("wrong email")
             return redirect(url_for("login"))
 
     return redirect(url_for("login"))
@@ -175,7 +185,11 @@ def dashboard():
         "completed_percentage": completed_percentage,
         "avg_booking_value": avg_booking_value
     }
-    return render_template("dashboard.html", title=title, stats=stats)
+    print(session.get("name"))
+    if session.get("name"):
+        return render_template("dashboard.html", title=title, stats=stats)
+    return redirect(url_for("login"))
+    
 
 
 @app.route("/team")
@@ -202,13 +216,15 @@ def team():
     pagination_team = get_pagination(data=team, page=page, offset=offset, per_page=per_page)
     pagination = Pagination(page=page, per_page=per_page, total=total)  
 
-    return render_template("team.html", 
+    if session.get("name"):
+        return render_template("team.html", 
                             admin=admin, 
                             team=pagination_team,
                             page=page,
                             per_page=per_page,
                             pagination=pagination,
                             title=title)
+    return redirect(url_for("login"))
 
 
 @app.route("/user/<id>")
@@ -217,7 +233,9 @@ def user(id):
     user = mongo.db.users.find_one(
             {"_id": ObjectId(id)})
     print(user)
-    return render_template("team-detail.html", title=title, user=user)
+    if session.get("name"):
+        return render_template("team-detail.html", title=title, user=user)
+    return redirect(url_for("login"))
 
 
 @app.route("/update_user/<id>", methods=["GET", "POST"])
@@ -253,32 +271,34 @@ def update_user(id):
 @app.route("/add_user", methods=["GET", "POST"])
 def add_user():
     title = "Add Team Member"
-    if request.method == "POST":
-        existing_user = mongo.db.users.find_one(
-            {"email": request.form.get("email").lower()})
+    if session.get("name"):
+        if request.method == "POST":
+            existing_user = mongo.db.users.find_one(
+                {"email": request.form.get("email").lower()})
 
-        if existing_user:
-            flash("User, " + existing_user["name"] + ", already has this email address")
-            return render_template("team-add.html", title=title)
+            if existing_user:
+                flash("User, " + existing_user["name"] + ", already has this email address")
+                return render_template("team-add.html", title=title)
 
+            else:
+                user = {
+                    "name": request.form.get("name"),
+                    "email": request.form.get("email").lower(),
+                    "position": request.form.get("position"),
+                    "access": request.form.get("access"),
+                    "password": generate_password_hash(request.form.get("password")),
+                    "created_by": session["email"],
+                    "created_date": datetime.today(),
+                    "updated_by": session["email"],
+                    "updated_date": datetime.today()
+                }
+                _id = mongo.db.users.insert_one(user).inserted_id
+
+                flash("User Added")
+                return redirect(url_for('user', id=_id))
         else:
-            user = {
-                "name": request.form.get("name"),
-                "email": request.form.get("email").lower(),
-                "position": request.form.get("position"),
-                "access": request.form.get("access"),
-                "password": generate_password_hash(request.form.get("password")),
-                "created_by": session["email"],
-                "created_date": datetime.today(),
-                "updated_by": session["email"],
-                "updated_date": datetime.today()
-            }
-            _id = mongo.db.users.insert_one(user).inserted_id
-
-            flash("User Added")
-            return redirect(url_for('user', id=_id))
-    else:
-        return render_template("team-add.html", title=title)
+            return render_template("team-add.html", title=title)
+    return redirect(url_for("login"))
 
 
 @app.route("/delete_user/<id>", methods=["GET", "POST"])
@@ -312,17 +332,19 @@ def guests(search=""):
     pagination_guests = get_pagination(data=guests, page=page, offset=offset, per_page=per_page)
     pagination = Pagination(page=page, per_page=per_page, total=total)  
 
-    if len(guests) == 1:
-        return guest(guests[0]["_id"])
-    else:
-        return render_template('guests.html',
-                            guests=pagination_guests,
-                            page=page,
-                            per_page=per_page,
-                            pagination=pagination,
-                            title=title,
-                            search=search
-                            )
+    if session.get("name"):
+        if len(guests) == 1:
+            return guest(guests[0]["_id"])
+        else:
+            return render_template('guests.html',
+                                guests=pagination_guests,
+                                page=page,
+                                per_page=per_page,
+                                pagination=pagination,
+                                title=title,
+                                search=search
+                                )
+    return redirect(url_for("login"))
 
 
 @app.route("/guest/<id>")
@@ -395,7 +417,8 @@ def guest(id):
 
     pagination_bookings = get_pagination(data=bookings, page=page, offset=offset, per_page=per_page)
     pagination = Pagination(page=page, per_page=per_page, total=total_bookings)
-    return render_template("guest-detail.html", 
+    if session.get("name"):
+        return render_template("guest-detail.html", 
                             guest=guest, 
                             bookings=pagination_bookings,
                             page=page,
@@ -403,6 +426,7 @@ def guest(id):
                             pagination=pagination, 
                             stats=stats, 
                             title=title)
+    return redirect(url_for("login"))
 
 
 @app.route("/update_guest/<id>", methods=["GET", "POST"])
@@ -450,42 +474,45 @@ def update_guest(id):
 @app.route("/add_guest", methods=["GET", "POST"])
 def add_guest():
     title = "Add Guest"
-    if request.method == "POST":
-        existing_user = mongo.db.clients.find_one(
+    if session.get("name"):
+        if request.method == "POST":
+            existing_user = mongo.db.clients.find_one(
             {"email": request.form.get("email").lower()})
 
-        if existing_user:
-            flash("Guest, " + existing_user["first_name"] + " " + existing_user["last_name"] + ", already has this email address")
-            return render_template("guest-add.html", title=title)
+            if existing_user:
+                flash("Guest, " + existing_user["first_name"] + " " + existing_user["last_name"] + ", already has this email address")
+                return render_template("guest-add.html", title=title)
 
+            else:
+                marketing_consent = "on" if request.form.get("marketingConsent") else "off"
+                dob = datetime.strptime(request.form.get("dob"), '%Y-%m-%d') if request.form.get("dob") else ""
+                client = {
+                    "first_name": request.form.get("first_name"),
+                    "last_name": request.form.get("last_name"),
+                    "email": request.form.get("email").lower(),
+                    "mobile": request.form.get("mobile"),
+                    "marketing_consent": marketing_consent,
+                    "rating": request.form.get("rating"),
+                    "dob": dob,
+                    "bookings": 0,
+                    "bookings_completed": 0,
+                    "value": 0,
+                    "notes_service": "",
+                    "notes_kitchen": "",
+                    "notes_allergies": "",
+                    "created_by": session["email"],
+                    "created_date": datetime.today(),
+                    "updated_by": session["email"],
+                    "updated_date": datetime.today()
+                }
+                _id = mongo.db.clients.insert_one(client).inserted_id
+
+                flash("Guest Added")
+                return redirect(url_for('guest', id=_id))
         else:
-            marketing_consent = "on" if request.form.get("marketingConsent") else "off"
-            dob = datetime.strptime(request.form.get("dob"), '%Y-%m-%d') if request.form.get("dob") else ""
-            client = {
-                "first_name": request.form.get("first_name"),
-                "last_name": request.form.get("last_name"),
-                "email": request.form.get("email").lower(),
-                "mobile": request.form.get("mobile"),
-                "marketing_consent": marketing_consent,
-                "rating": request.form.get("rating"),
-                "dob": dob,
-                "bookings": 0,
-                "bookings_completed": 0,
-                "value": 0,
-                "notes_service": "",
-                "notes_kitchen": "",
-                "notes_allergies": "",
-                "created_by": session["email"],
-                "created_date": datetime.today(),
-                "updated_by": session["email"],
-                "updated_date": datetime.today()
-            }
-            _id = mongo.db.clients.insert_one(client).inserted_id
-
-            flash("Guest Added")
-            return redirect(url_for('guest', id=_id))
-    else:
-        return render_template("guest-add.html", title=title)
+            return render_template("guest-add.html", title=title)
+    return redirect(url_for("login"))
+    
 
 
 @app.route("/delete_guest/<id>", methods=["GET", "POST"])
@@ -537,8 +564,9 @@ def bookings(date="", status=""):
         for y in range(len(clients)):
             if str(clients[y]["_id"]) == str(bookings[x]["client_id"]):
                 bookings[x]["full_name"] = clients[y]["first_name"] + " " + clients[y]["last_name"]
-        
-    return render_template("bookings.html", bookings=bookings, title=title, date=date, status=status)
+    if session.get("name"):
+        return render_template("bookings.html", bookings=bookings, title=title, date=date, status=status)
+    return redirect(url_for("login"))
 
 
 @app.route("/booking/<id>", methods=["GET", "POST"])
@@ -555,7 +583,10 @@ def booking(id):
     booking["written_date"] = written_date.strftime("%a %d %b")
     booking["full_name"] = guest["first_name"] + " " + guest["last_name"]
     booking["rating"] = int(booking["rating"])
-    return render_template("booking-detail.html", guest=guest, booking=booking, title=title)
+    if session.get("name"):
+        return render_template("booking-detail.html", guest=guest, booking=booking, title=title)
+    return redirect(url_for("login"))
+    
 
 
 @app.route("/update_booking/<id>", methods=["GET", "POST"])
