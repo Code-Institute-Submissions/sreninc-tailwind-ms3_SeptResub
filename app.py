8758e75142fa5ab347d78a26f250a065cf7bc2bf
sreninc <sreninc@gmail.com>
2021-07-06 19:00:42 +0000

    FEAT: Disabled debug mode in app.py

--- app.py
+++ app.py
@@ -807,4 +807,4 @@ def delete_booking(id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
